84c6f5212015a452dee7aff1730b6b2e5a0c3eb3
Jerry Moua <jerrymoua@gmail.com>
2019-05-28 21:13:49 -0500

    adding server.js

--- config/connections.js
+++ config/connections.js
@@ -3,16 +3,14 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+} else {
     connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,    
-        user: "root",
-        password: "apple",
-        database: "burgers_db"
+        host: 'localhost',
+        user: 'root',
+        password: 'apple',
+        database: 'burgers_db'
     });
 }
 
